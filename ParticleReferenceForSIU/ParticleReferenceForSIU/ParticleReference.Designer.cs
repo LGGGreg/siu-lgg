f5739185f1725166fa9c7c978e2d90763c311c24
AdAstley <adastley@gmail.com>
2011-11-23 09:18:40 -0600

    adding particle frame back to particle reference sln

--- ParticleReferenceForSIU/ParticleReferenceForSIU/ParticleReference.Designer.cs
+++ ParticleReferenceForSIU/ParticleReferenceForSIU/ParticleReference.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ParticleReferenceForSIU
+﻿namespace SkinInstaller
 {
     partial class ParticleReference
     {
@@ -30,6 +30,9 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox = new System.Windows.Forms.RichTextBox();
+            this.particleReaderWorker = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -50,17 +53,43 @@
             this.textBox.TabIndex = 1;
             this.textBox.Text = "";
             // 
+            // particleReaderWorker
+            // 
+            this.particleReaderWorker.WorkerReportsProgress = true;
+            this.particleReaderWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.particleReaderWorker_DoWork);
+            this.particleReaderWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.particleReaderWorker_ProgressChanged);
+            this.particleReaderWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.particleReaderWorker_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(35, 36);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(528, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(569, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(208, 56);
+            this.textBox1.TabIndex = 3;
+            // 
             // ParticleReference
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(789, 792);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.button1);
             this.Name = "ParticleReference";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.ParticleReference_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +97,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox textBox;
+        private System.ComponentModel.BackgroundWorker particleReaderWorker;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ParticleReferenceForSIU/ParticleReferenceForSIU/ParticleReference.cs
+++ ParticleReferenceForSIU/ParticleReferenceForSIU/ParticleReference.cs
@@ -9,8 +9,9 @@ using System.Windows.Forms;
 using System.IO;
 using RAFLib;
 using System.Text.RegularExpressions;
+using SkinInstaller;
 
-namespace ParticleReferenceForSIU
+namespace SkinInstaller
 {
     public partial class ParticleReference : Form
     {
@@ -19,14 +20,30 @@ namespace ParticleReferenceForSIU
             InitializeComponent();
         }
 
+        public class PStruct : Dictionary
+                <String, Dictionary
+                    <String, Dictionary
+                        <RAFFileListEntry, List<RAFFileListEntry>>>> { }
+        //public skinInstaller mySkinInstaller = null;
+        public int lastProgress = 0;
+        string progs = "";
+
         private void ParticleReference_Load(object sender, EventArgs e)
         {
             
         }
-        public Dictionary
-                <String, Dictionary
-                    <String, Dictionary
-                        <RAFFileListEntry, List<RAFFileListEntry>>>> getParticleStructure(string rafPath)
+        public void reportProgress(int p)
+        {
+            if (p != lastProgress)
+            {
+                lastProgress = p;
+
+                particleReaderWorker.ReportProgress(p);
+                progs += "|" + p.ToString() + "|";
+                
+            }
+        }
+        public PStruct getParticleStructure(string rafPath)
         {
 
             List<RAFFileListEntry> fileList = new List<RAFFileListEntry>();
@@ -35,7 +52,9 @@ namespace ParticleReferenceForSIU
             Directory.GetDirectories(baseDir);
             string[] array = files;
             List<String> rafFiles = new List<string>();
-            for (int i = 0; i < array.Length; i++)
+            reportProgress(1);
+            int i = 0;
+            for (i = 0; i < array.Length; i++)
             {
                 FileInfo fileInfo = new FileInfo(array[i]);
 
@@ -46,25 +65,32 @@ namespace ParticleReferenceForSIU
                         rafFiles.Add(fileInfo.FullName);//add raf files to read afterwards
                     }
                 }
+                reportProgress((int)(((double)i * 10.0) / (double)array.Length)+1);
             }
-
+            reportProgress(12);
+            i = 0;
+            List<RAFFileList> listsToSearchLater = new List<RAFFileList>();
             foreach (String file in rafFiles)
             {
+                i++;
                 FileInfo rafFile = new FileInfo(file);
                 //time to process the raf files
                 RAFArchive raf = new RAFArchive(rafFile.FullName);
                 fileList.AddRange(raf.GetDirectoryFile().GetFileList().GetFileEntries());
+                listsToSearchLater.Add(raf.GetDirectoryFile().GetFileList());
                 raf.GetDataFileContentStream().Close();
+                reportProgress((int)(((double)i * 10.0) / (double)rafFiles.Count) + 12);
             }
+            reportProgress(25);
 
-            Dictionary<String, Dictionary
-                <String, Dictionary
-                    <RAFFileListEntry, List<RAFFileListEntry>>>> particleDef = new Dictionary<String, Dictionary<String, Dictionary<RAFFileListEntry, List<RAFFileListEntry>>>>();
+            PStruct particleDef = new PStruct();
             List<String> exceptions = new List<String>();
-
+            i = 0;
             // Reference spellnames to champion names
             foreach ( RAFFileListEntry file in fileList)
             {
+                i++;
+                reportProgress((int)(((double)i/ (double)fileList.Count) * (double)10.0)+ 25);
                 // Only use spell icon files
                 if (file.FileName.Contains("DATA/Spells/Icons2D"))
                 {
@@ -94,11 +120,11 @@ namespace ParticleReferenceForSIU
                             // Find index of second uppercase letter
                             int splitIndex = 0;
                             char[] charArray = iconFileName.ToCharArray();
-                            for (int i = 1; i < charArray.Length; i++)
+                            for (int ii = 1; ii < charArray.Length; ii++)
                             {
-                                if (char.IsUpper(charArray[i]))
+                                if (char.IsUpper(charArray[ii]))
                                 {
-                                    splitIndex = i;
+                                    splitIndex = ii;
                                     break;
                                 }
                             }
@@ -159,10 +185,13 @@ namespace ParticleReferenceForSIU
             //
 
             int troybinTotal = 0;
-
+            i = 0;
+            reportProgress(36);
             // Search for troybins and inibins that correspond to the champion names
             foreach (RAFFileListEntry file in fileList)
             {
+                i++;
+                reportProgress((int)(((double)i * 10.0) / (double)fileList.Count) + 36);
                 if (file.FileName.Contains("DATA/Particles"))
                 {
                     String particleFileName = file.FileName.ToLower();
@@ -189,10 +218,13 @@ namespace ParticleReferenceForSIU
             }
 
             List<RAFFileListEntry> leftoverTroybins = new List<RAFFileListEntry>();
-
+            i = 0;
+            reportProgress(47);
             // Search for troybins and inibins that correspond to the champion spells
             foreach (RAFFileListEntry troybin in missedTroybins)
             {
+                i++;
+                reportProgress((int)(((double)i * 10.0) / (double)missedTroybins.Count) + 47);
                 Boolean matchFound = false;
                 foreach (KeyValuePair<String, Dictionary<String, Dictionary<RAFFileListEntry, List<RAFFileListEntry>>>> championKVP in particleDef)
                 {
@@ -214,9 +246,12 @@ namespace ParticleReferenceForSIU
                 if (!matchFound)
                     leftoverTroybins.Add(troybin);
             }
-
+            reportProgress(58);
+            i = 0;
             foreach (KeyValuePair<String, Dictionary<String, Dictionary<RAFFileListEntry, List<RAFFileListEntry>>>> championKVP in particleDef)
             {
+                reportProgress((int)(((double)i * 40) / (double)particleDef.Count) + 58);
+                i++;
                 foreach (KeyValuePair<RAFFileListEntry, List<RAFFileListEntry>> troybinKVP in particleDef[championKVP.Key]["troybins"])
                 {
                     // Search troybins for .dds, .sco, .scb, etc.
@@ -229,49 +264,141 @@ namespace ParticleReferenceForSIU
                     String result = reader.ReadToEnd();
                     reader.Close();
                     myInput.Close();
-
                     // Release the archive
                     rafArchive.GetDataFileContentStream().Close();
+
+                    String cleanString = Regex.Replace(result, @"[^\u0000-\u007F]", "").Replace('\0','?');
+                    Regex captureFileNames = new Regex(@"([a-zA-z0-9\-_ ]+\.(?:tga|sco|scb|dds|png))",RegexOptions.IgnoreCase);
+                    MatchCollection matches = captureFileNames.Matches(cleanString);
+                    foreach (Match match in matches)
+                    {
+                        string matchedFile = "/" + match.Groups[1].Value;
+
+                        //foreach (String file in rafFiles)
+                        //{
+                        foreach(RAFFileList listToSearchNow in listsToSearchLater)
+                        {
+                            //FileInfo rafFile = new FileInfo(file);
+                            //time to process the raf files
+                            //RAFArchive searchRaf = new RAFArchive(rafFile.FullName);
+
+                            //searching takes forever, F that
+                            //troybinKVP.Value.AddRange(listToSearchNow.SearchFileEntries(matchedFile));
+                            
+                            //damnit this also takes freking forever!
+                            /*
+                            bool found = false;
+                            RAFFileListEntry entry =
+                                    listToSearchNow.GetFileEntry("DATA/Particles" + matchedFile);
+                            if (entry != null)
+                            {
+                                troybinKVP.Value.Add(entry);
+                                found = true;
+                            }
+                            if (matchedFile.ToLower().Contains(".tga"))
+                            {
+                                entry =
+                                    listToSearchNow.GetFileEntry("DATA/Particles" + matchedFile.ToLower().Replace(".tga",".dds"));
+                                if (entry != null)
+                                {
+                                    troybinKVP.Value.Add(entry);
+                                    found = true;
+                                }
+                            }
+                            if (!found)
+                            {
+                                //todo look at not found files and see whats up
+                            }*/
+                        }
+
+                        //screw it, just fake a entry :|
+                        //in fact.. ill do the final search thingy when they actually export!
+                        RAFFileListEntry entry = new RAFFileListEntry(null, "DATA/Particles" + 
+                            matchedFile.Replace(".tga",".dds").Replace(".TGA",".DDS"), 0,0,0);
+                        troybinKVP.Value.Add(entry);
+                        cleanString = matchedFile + "\r\n" + cleanString;
+                        //searchRaf.GetDataFileContentStream().Close();
+                        
+                    }
+                    if (textBox1.Text == "") textBox.AppendText(cleanString);
+
+                    
+
                 }
             }
+            reportProgress(100);
 
 
             return particleDef;
         }
-
-
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            Dictionary
-                <String, Dictionary
-                    <String, Dictionary
-                        <RAFFileListEntry, List<RAFFileListEntry>>>>
-                            particleDef = getParticleStructure("C:\\Riot Games\\League of Legends\\RADS\\projects\\lol_game_client\\filearchives\\");
+            button1.Enabled = false;
+            particleReaderWorker.RunWorkerAsync("C:\\Riot Games\\League of Legends\\RADS\\projects\\lol_game_client\\filearchives\\");
+        }
+        //public void startGettingParticleStructure(skinInstaller source, string rafDirPath)
+        //{
+        //    mySkinInstaller = source;
+        //    particleReaderWorker.RunWorkerAsync(rafDirPath);       
+        //}
 
-            //int troybinCount = 0;
-            // Display particleDef for debugging purposes
-            foreach (KeyValuePair<String, Dictionary<String, Dictionary<RAFFileListEntry, List<RAFFileListEntry>>>> championKVP in particleDef)
+        private void particleReaderWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            e.Result = getParticleStructure((string)e.Argument);
+        }
+
+        private void particleReaderWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //if (mySkinInstaller != null)
+            //{
+            //    mySkinInstaller.recieveParticleProgress(e.ProgressPercentage);
+            //}
+            if (this.Visible)
             {
-                textBox.AppendText(championKVP.Key + "->>\n");
-                textBox.AppendText("\tSpell->>\n");
-                foreach (KeyValuePair<String, Dictionary<RAFFileListEntry, List<RAFFileListEntry>>> spellKVP in particleDef[championKVP.Key])
+                progressBar1.Value = e.ProgressPercentage;
+                progressBar1.Refresh();
+            }
+        }
+
+        private void particleReaderWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            PStruct particleDef =(PStruct)e.Result;
+            //int troybinCount = 0;
+            if (this.Visible)
+            {
+                textBox.AppendText(progs + "\r\n\r\n");
+                this.button1.Enabled = true;
+                // Display particleDef for debugging purposes
+                foreach (KeyValuePair<String, Dictionary<String, Dictionary<RAFFileListEntry, List<RAFFileListEntry>>>> championKVP in particleDef)
                 {
-                    if (spellKVP.Key != "troybins")
+                    textBox.AppendText(championKVP.Key + "->>\n");
+                    textBox.AppendText("\tSpell->>\n");
+                    foreach (KeyValuePair<String, Dictionary<RAFFileListEntry, List<RAFFileListEntry>>> spellKVP in particleDef[championKVP.Key])
                     {
-                        textBox.AppendText("\t\t" + spellKVP.Key + "\n");
+                        if (spellKVP.Key != "troybins")
+                        {
+                            textBox.AppendText("\t\t" + spellKVP.Key + "\n");
+                        }
+                    }
+                    textBox.AppendText("\tTroybins->>\n");
+                    foreach (KeyValuePair<RAFFileListEntry, List<RAFFileListEntry>> troybinKVP in particleDef[championKVP.Key]["troybins"])
+                    {
+                        textBox.AppendText("\t\t" + troybinKVP.Key.FileName + "\n");
+                        //troybinCount++;
+                        foreach (RAFFileListEntry fileEntry in troybinKVP.Value)
+                        {
+                            textBox.AppendText("\t\t\t" + fileEntry.FileName + "\n");
+                        }
                     }
                 }
-                textBox.AppendText("\tTroybins->>\n");
-                foreach (KeyValuePair<RAFFileListEntry, List<RAFFileListEntry>> troybinKVP in particleDef[championKVP.Key]["troybins"])
-                {
-                    textBox.AppendText("\t\t" + troybinKVP.Key.FileName + "\n");
-                    //troybinCount++;
-                }
             }
+            //if (mySkinInstaller != null)
+            //{
+            //    mySkinInstaller.recieveParticleInformation(particleDef);
+            //}
             //int temp = troybinCount + leftoverTroybins.Count;
             textBox.AppendText("Troybins identified: ");// + troybinCount);
-
         }
     }
 }
