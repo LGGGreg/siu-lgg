0572033a7b3f3df10a7fa906cb21612bdbcf9747
LordGregGreg <LordGregGreg@gmail.com>
2011-11-23 23:29:43 -0600

    blegh

--- ParticleReferenceForSIU/ParticleReferenceForSIU/Program.cs
+++ ParticleReferenceForSIU/ParticleReferenceForSIU/Program.cs
@@ -16,7 +16,7 @@ namespace SkinInstaller
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ParticleReference());
+            Application.Run(new PartRef.ParticleReference());
         }
     }
 }
