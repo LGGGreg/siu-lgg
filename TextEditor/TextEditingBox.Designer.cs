061e40d2afa1454633af8ee52896b308f7781eca
LordGregGreg <LordGregGreg@gmail.com>
2011-12-19 23:34:10 -0600

    Part 1 Merging texteditor into SIU

--- TextEditor/TextEditingBox.Designer.cs
+++ TextEditor/TextEditingBox.Designer.cs
@@ -32,22 +32,25 @@
             this.TextDiscardButton = new System.Windows.Forms.Button();
             this.textAcceptButton = new System.Windows.Forms.Button();
             this.editTips_lbl = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // richTextBox
             // 
-            this.richTextBox.Location = new System.Drawing.Point(21, 22);
+            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox.Location = new System.Drawing.Point(0, 0);
             this.richTextBox.Name = "richTextBox";
-            this.richTextBox.Size = new System.Drawing.Size(754, 236);
+            this.richTextBox.Size = new System.Drawing.Size(796, 273);
             this.richTextBox.TabIndex = 0;
             this.richTextBox.Text = "";
             // 
             // TextDiscardButton
             // 
             this.TextDiscardButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.TextDiscardButton.Location = new System.Drawing.Point(475, 270);
+            this.TextDiscardButton.Location = new System.Drawing.Point(440, 3);
             this.TextDiscardButton.Name = "TextDiscardButton";
-            this.TextDiscardButton.Size = new System.Drawing.Size(123, 37);
+            this.TextDiscardButton.Size = new System.Drawing.Size(123, 38);
             this.TextDiscardButton.TabIndex = 1;
             this.TextDiscardButton.Text = "Discard Changes";
             this.TextDiscardButton.UseVisualStyleBackColor = true;
@@ -55,7 +58,7 @@
             // textAcceptButton
             // 
             this.textAcceptButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.textAcceptButton.Location = new System.Drawing.Point(625, 270);
+            this.textAcceptButton.Location = new System.Drawing.Point(569, 3);
             this.textAcceptButton.Name = "textAcceptButton";
             this.textAcceptButton.Size = new System.Drawing.Size(123, 37);
             this.textAcceptButton.TabIndex = 2;
@@ -67,28 +70,38 @@
             this.editTips_lbl.AutoSize = true;
             this.editTips_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.editTips_lbl.ForeColor = System.Drawing.Color.Black;
-            this.editTips_lbl.Location = new System.Drawing.Point(18, 268);
+            this.editTips_lbl.Location = new System.Drawing.Point(3, 0);
             this.editTips_lbl.Name = "editTips_lbl";
             this.editTips_lbl.Size = new System.Drawing.Size(431, 39);
             this.editTips_lbl.TabIndex = 3;
             this.editTips_lbl.Text = "Do not edit any of the tags. Ex: <titleLeft>\r\nAnything enclosed with @ are dynami" +
-                "c values in-game. Use them how you \r\n    wish, but I can\'t guarantee them workin" +
-                "g out of context.";
+    "c values in-game. Use them how you \r\n    wish, but I can\'t guarantee them workin" +
+    "g out of context.";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.editTips_lbl);
+            this.panel1.Controls.Add(this.textAcceptButton);
+            this.panel1.Controls.Add(this.TextDiscardButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 273);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(796, 46);
+            this.panel1.TabIndex = 4;
             // 
             // TextEditingBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(796, 319);
-            this.Controls.Add(this.editTips_lbl);
-            this.Controls.Add(this.textAcceptButton);
-            this.Controls.Add(this.TextDiscardButton);
             this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "TextEditingBox";
             this.Text = "Text Editing Box";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -98,5 +111,6 @@
         private System.Windows.Forms.Button TextDiscardButton;
         private System.Windows.Forms.Button textAcceptButton;
         private System.Windows.Forms.Label editTips_lbl;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- TextEditor/TextEditorMain.Designer.cs
+++ TextEditor/TextEditorMain.Designer.cs
@@ -37,22 +37,35 @@
             this.filter_txt = new System.Windows.Forms.TextBox();
             this.filterButton = new System.Windows.Forms.Button();
             this.editedTextClearButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // textInstallButton
             // 
-            this.textInstallButton.Location = new System.Drawing.Point(32, 29);
+            this.textInstallButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textInstallButton.Location = new System.Drawing.Point(481, 3);
             this.textInstallButton.Name = "textInstallButton";
             this.textInstallButton.Size = new System.Drawing.Size(95, 36);
             this.textInstallButton.TabIndex = 0;
             this.textInstallButton.Text = "Install Text";
             this.textInstallButton.UseVisualStyleBackColor = true;
+            this.textInstallButton.Visible = false;
             this.textInstallButton.Click += new System.EventHandler(this.textInstallButton_Click);
             // 
             // exportButton
             // 
             this.exportButton.Enabled = false;
-            this.exportButton.Location = new System.Drawing.Point(234, 29);
+            this.exportButton.Location = new System.Drawing.Point(3, 4);
             this.exportButton.Name = "exportButton";
             this.exportButton.Size = new System.Drawing.Size(110, 36);
             this.exportButton.TabIndex = 1;
@@ -62,33 +75,38 @@
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(32, 101);
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 23);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(275, 425);
+            this.treeView1.Size = new System.Drawing.Size(315, 485);
             this.treeView1.TabIndex = 2;
             this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.OrigNodeMouseDoubleClick);
             // 
             // treeView2
             // 
-            this.treeView2.Location = new System.Drawing.Point(365, 101);
+            this.treeView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView2.Location = new System.Drawing.Point(0, 23);
             this.treeView2.Name = "treeView2";
-            this.treeView2.Size = new System.Drawing.Size(275, 425);
+            this.treeView2.Size = new System.Drawing.Size(350, 485);
             this.treeView2.TabIndex = 3;
+            this.treeView2.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView2_AfterSelect);
             this.treeView2.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.EditedNodeMouseDoubleClick);
             // 
             // textUninstallButton
             // 
-            this.textUninstallButton.Location = new System.Drawing.Point(133, 29);
+            this.textUninstallButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textUninstallButton.Location = new System.Drawing.Point(579, 4);
             this.textUninstallButton.Name = "textUninstallButton";
             this.textUninstallButton.Size = new System.Drawing.Size(95, 36);
             this.textUninstallButton.TabIndex = 4;
             this.textUninstallButton.Text = "Uninstall Text";
             this.textUninstallButton.UseVisualStyleBackColor = true;
+            this.textUninstallButton.Visible = false;
             this.textUninstallButton.Click += new System.EventHandler(this.textUninstallButton_Click);
             // 
             // importButton
             // 
-            this.importButton.Location = new System.Drawing.Point(350, 29);
+            this.importButton.Location = new System.Drawing.Point(119, 4);
             this.importButton.Name = "importButton";
             this.importButton.Size = new System.Drawing.Size(110, 36);
             this.importButton.TabIndex = 5;
@@ -98,17 +116,18 @@
             // 
             // filter_txt
             // 
-            this.filter_txt.Location = new System.Drawing.Point(484, 38);
+            this.filter_txt.Location = new System.Drawing.Point(242, 13);
             this.filter_txt.Name = "filter_txt";
             this.filter_txt.Size = new System.Drawing.Size(124, 20);
             this.filter_txt.TabIndex = 6;
+            this.filter_txt.TextChanged += new System.EventHandler(this.filter_txt_TextChanged);
             this.filter_txt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.filter_txt_KeyDown);
             // 
             // filterButton
             // 
-            this.filterButton.Location = new System.Drawing.Point(614, 35);
+            this.filterButton.Location = new System.Drawing.Point(368, 13);
             this.filterButton.Name = "filterButton";
-            this.filterButton.Size = new System.Drawing.Size(47, 24);
+            this.filterButton.Size = new System.Drawing.Size(56, 20);
             this.filterButton.TabIndex = 7;
             this.filterButton.Text = "Filter";
             this.filterButton.UseVisualStyleBackColor = true;
@@ -116,7 +135,8 @@
             // 
             // editedTextClearButton
             // 
-            this.editedTextClearButton.Location = new System.Drawing.Point(570, 68);
+            this.editedTextClearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.editedTextClearButton.Location = new System.Drawing.Point(256, 0);
             this.editedTextClearButton.Name = "editedTextClearButton";
             this.editedTextClearButton.Size = new System.Drawing.Size(91, 23);
             this.editedTextClearButton.TabIndex = 8;
@@ -124,25 +144,78 @@
             this.editedTextClearButton.UseVisualStyleBackColor = true;
             this.editedTextClearButton.Click += new System.EventHandler(this.editedTextClearButton_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textInstallButton);
+            this.panel1.Controls.Add(this.importButton);
+            this.panel1.Controls.Add(this.filterButton);
+            this.panel1.Controls.Add(this.exportButton);
+            this.panel1.Controls.Add(this.filter_txt);
+            this.panel1.Controls.Add(this.textUninstallButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(673, 43);
+            this.panel1.TabIndex = 9;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 43);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Panel1.Controls.Add(this.panel3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.treeView2);
+            this.splitContainer1.Panel2.Controls.Add(this.panel2);
+            this.splitContainer1.Size = new System.Drawing.Size(673, 510);
+            this.splitContainer1.SplitterDistance = 317;
+            this.splitContainer1.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(315, 23);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.editedTextClearButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(350, 23);
+            this.panel2.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
             // TextEditorMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(673, 553);
-            this.Controls.Add(this.editedTextClearButton);
-            this.Controls.Add(this.filterButton);
-            this.Controls.Add(this.filter_txt);
-            this.Controls.Add(this.importButton);
-            this.Controls.Add(this.textUninstallButton);
-            this.Controls.Add(this.treeView2);
-            this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.exportButton);
-            this.Controls.Add(this.textInstallButton);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
             this.Name = "TextEditorMain";
             this.Text = "Text Editor";
             this.Load += new System.EventHandler(this.TextEditorMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -157,6 +230,12 @@
         private System.Windows.Forms.TextBox filter_txt;
         private System.Windows.Forms.Button filterButton;
         private System.Windows.Forms.Button editedTextClearButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
